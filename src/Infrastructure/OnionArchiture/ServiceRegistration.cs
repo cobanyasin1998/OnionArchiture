70e0f18583c4d0430fe7f0f3ea808f2ac5ab3855
Yasin <yasincoban1998@outlook.com>
2022-11-17 18:04:54 +0300

    finish basic architure

---
+++ src/Infrastructure/OnionArchiture/ServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnionArchiture.Application.Interfaces.Repository;
+using OnionArchiture.Persistence.Context;
+using OnionArchiture.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchiture.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseInMemoryDatabase("memoryDb");
+            });
+
+            serviceCollection.AddTransient<IProductRepository, ProductRepository>();
+         
+        }
+    }
+}
---
+++ src/WebApi/OnionArchiture.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionArchiture.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnionArchiture.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        //[HttpGet]
+        //public async Task<IActionResult> Get()
+        //{
+        //    var allList = await _productRepository.GetAllAsync();
+        //    return allList;
+        //}
+
+    }
+}
